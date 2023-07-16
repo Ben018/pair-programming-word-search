5f1ad69337ec3e22a410a6647e6a0255f7931673
Benson <bensonwlee@gmail.com>
2023-07-16 16:38:14 -0400

    Word Search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+function wordSearch(letters, word) {
+    var horizontalJoin = letters.map(function (ls) {
+        return ls.join('');
+    });
+
+    var verticalJoin = transpose(letters).map(function (ls) {
+        return ls.join('');
+    });
+
+    for (var i = 0; i < horizontalJoin.length; i++) {
+        if (horizontalJoin[i].includes(word)) {
+            return true;
+        }
     }
+
+    for (var j = 0; j < verticalJoin.length; j++) {
+        if (verticalJoin[j].includes(word)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function transpose(matrix) {
+    return matrix[0].map(function (col, i) {
+        return matrix.map(function (row) {
+            return row[i];
+        });
+    });
 }
 
 module.exports = wordSearch
